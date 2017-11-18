f58477b0ece8ca659403be98ecc63228b3720a45
VienNguyen <luongvien13t2@gmail.com>
2017-11-18 23:28:48 +0700

    api

--- Travel/routes/api.php
+++ Travel/routes/api.php
@@ -18,3 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/get-districts', 'AccountController@getDistrictByProvinceId');
+Route::get('/events', 'ApiController@getEvents');
+Route::get('/places', 'ApiController@getPlaces');
